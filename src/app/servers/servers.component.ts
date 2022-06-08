21300c8c2a0c3f91ca66e981bffff67338346eb5
nikabramishvili <n.abramishvili@area.ge>
2022-06-08 21:04:25 +0400

    prog

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.scss'],
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationstatus = 'No server was created';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationstatus = 'Server was created';
+  }
+}
