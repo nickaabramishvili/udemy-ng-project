19d7f2a272c0734ea7a11b96f81020fbd010f808
nikabramishvili <n.abramishvili@area.ge>
2022-06-11 11:42:03 +0400

    prog

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationstatus = 'No server was created';
   serverName = 'ss';
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -20,6 +21,7 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationstatus =
       'Server was created and name is ' + this.serverName;
+    this.serverCreated = true;
   }
   onUpdateServerName(event: Event) {
     console.log(event);
